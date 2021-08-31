0223b41802cc3a69565ee04b13844cb239347660
Beytullah Taskin <beytullah.taskin@arvato-scs.com>
2021-08-31 23:23:21 +0300

    .

---
+++ Cauchbase.Console/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using App.Metrics.Serialization;
+using Couchbase;
+using Couchbase.Extensions.DependencyInjection;
+
+namespace Cauchbase.Console
+{
+    public class Program
+
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World!");
+        }
+    }
+
+    public class TestEntity
+    {
+        public string Id { get; set; }
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public string Desc { get; set; }
+    }
+}
